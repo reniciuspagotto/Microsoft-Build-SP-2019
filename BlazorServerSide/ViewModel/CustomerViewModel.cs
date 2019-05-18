c09aa4964b9a6750c6a816acb75f129828957cec
Renicius Pagotto <renicius.pagotto@outlook.com>
2019-05-18 17:08:32 -0300

    some init msg

---
+++ BlazorServerSide/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorServerSide.ViewModel
+{
+    public class CustomerViewModel
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public int Idade { get; set; }
+    }
+}
